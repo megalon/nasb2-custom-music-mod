482169c27316fd9272e34d84e6429a055aeb40a2
megalon <megalongaming@gmail.com>
2023-11-17 16:56:47 -0800

    Version bump v0.4.0

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -8,6 +8,6 @@ namespace NickCustomMusicMod
     {
         public const string PLUGIN_GUID = "megalon.nasb2_custom_music_mod";
         public const string PLUGIN_NAME = "NASB2CustomMusicMod";
-        public const string PLUGIN_VERSION = "0.3.1";
+        public const string PLUGIN_VERSION = "0.4.0";
     }
 }
